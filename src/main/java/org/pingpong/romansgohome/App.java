0fc0b2b3fe9bdfb3c0f71a8d470d44c99b05a1bb
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-05-02 18:10:26 +0200

    perf: Perfect code structure of main and check if main works

--- src/main/java/org/pingpong/romansgohome/App.java
+++ src/main/java/org/pingpong/romansgohome/App.java
@@ -13,18 +13,19 @@ public class App
     public static void main( String[] args )
     {
         String test = "MMMDCCCLXXXVIII"; // 3888
-        test = "MMDCCLXXVII";  // 2777
-        test = "CDXLIV"; // 444
+        //test = "MMDCCLXXVII";  // 2777
+        //test = "CDXLIV"; // 444
         test = "CDXXXIX"; // 439
 
         TranslatorRomanNumbers romanNumber = new TranslatorRomanNumbers(test);
         //romanNumber.initRegexDictionary();
-        romanNumber.addRegex("grupoSumatorio", "(?<!C)[DM]|(?<!X)[LC](?![DM])|(?<!I)[VX](?![LC])|I(?![VX])");
+        romanNumber.addRegex("grupoSumatorio", "((?<!C)[DM]|(?<!X)[LC](?![DM]))|((?<!I)[VX](?![LC])|I(?![VX]))");
         romanNumber.addRegex("grupoSustractivo", "(C[DM])|(X[LC])|(I[VX])");
-
-        assertThat(romanNumber.toDecimal()).isEqualTo((short) 439);
-
-        System.out.println((romanNumber.getDecimalNumber() != 0) ? romanNumber.getRomanNumber() + " = " +  romanNumber.toDecimal() : "The roman number is incorrect");
+        //romanNumber.initRegexDictionary();
+        //assertThat(romanNumber.toDecimal()).isEqualTo((short) 439);
+        System.out.printf("%s = %d", romanNumber.getRomanNumber(), romanNumber.toDecimal());
+        // La linea siguiente dará error debido a que ya previamente habiamos llamado al método toDecimal()
+        //System.out.println((romanNumber.toDecimal() != 0) ? romanNumber.getRomanNumber() + " = " +  romanNumber.toDecimal() : "The roman number is incorrect");
 
     }
 }
